8e1f765c72982395ca4f50a9b8521ac4a126bae9
Syxsaran <96071669+Syxsaran@users.noreply.github.com>
2021-12-16 16:47:14 +0700

    save
    update

--- proctof.cs
+++ proctof.cs
@@ -34,5 +34,19 @@ namespace proctof
             double F = C * 9 / 5 + 32;
             textBox2.Text = F.ToString();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string input = textBox2.Text;
+            double F = Convert.ToDouble(input);
+            double C = (F - 32) / 1.8;
+            textBox1.Text = C.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
     }
 }
